1716214a9fd1166659e59f3874921bb2e794a2b3
rational-architecture-1641 <jarmah241@gmail.com>
2020-10-08 18:16:09 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,13 +3,23 @@
 def game_hash
   team_data = {
     :home => {
-      :team_name => "",
-      :colors => ["", ""],
-      :players => []
+      :team_name => "Brooklyn Nets",
+      :colors => ["Black", "White"],
+      :players => [{
+        :player_name => "Alan Anderson",
+        :number => 0,
+        :shoe => 16,
+        :points => 22,
+        :rebounds => 12,
+        :assists => 12,
+        :steals => 3,
+        :blocks => 1,
+        :slam_dunks => 1
+      }]
     },
     :away => {
-      :team_name => "",
-      :colors => ["", ""],
+      :team_name => "Charlotte Hornets",
+      :colors => ["Turquoise", "Purple"],
       :players => []
     }
   }
